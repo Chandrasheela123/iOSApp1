83a9f2c7e603bff61a45436ec63d20a5e21c0839
Chandrasheela123 <chandrasheela1927@gmail.com>
2022-06-06 14:54:29 +0530

    Added alert function

--- GitDemo/Utilities.swift
+++ GitDemo/Utilities.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController{
+
+func showAlert(title: String, msg : String){
+    
+    
+    let alertVC = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+    
+    let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+    
+    alertVC.addAction(okAction)
+    present(alertVC, animated: false, completion: nil)
+    
+}
+}
